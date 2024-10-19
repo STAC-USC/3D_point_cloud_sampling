ee57c3a5e61a3cbc93c4f0b87bc5787a669c3980
Shashank N Sridhara <nsshashank95@gmail.com>
2024-10-19 11:50:23 -0700

    Update no_self_loops.m

--- no_self_loops.m
+++ no_self_loops.m
@@ -1,3 +1,10 @@
+% Graph-based scalable sampling of 3D point cloud attributes
+% IEEE. Transactions on Image Processing
+% Author: Shashank N. Sridhara, University of Southern California
+
+%Function to compute self-loop weights for each block (zero self loop weights in this case)
+
+
 function loop_wts_mat = no_self_loops(A, block_logical)
     n_block = nnz(block_logical);
     loop_wts_mat = sparse(n_block, n_block);
