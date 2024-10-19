c08ade4a3eaeef953e9c3f99873ab9c89533eab9
Shashank N Sridhara <nsshashank95@gmail.com>
2024-10-19 11:51:02 -0700

    Update index_bounds_to_logical_arr.m

--- Utils/index_bounds_to_logical_arr.m
+++ Utils/index_bounds_to_logical_arr.m
@@ -1,3 +1,9 @@
+% Graph-based scalable sampling of 3D point cloud attributes
+% IEEE. Transactions on Image Processing
+% Author: Shashank N. Sridhara, University of Southern California
+
+
+
 function final_arr = index_bounds_to_logical_arr(N, index_bounds)
     % Given the size of the final array and the indices - index_bounds[1]
     % and index_bounds[2] in which the final array should be true(and false
