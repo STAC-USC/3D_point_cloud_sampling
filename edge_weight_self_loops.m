2e8255301cbcc2a1e47b15f39dcb456a6f7f7b16
Shashank N Sridhara <nsshashank95@gmail.com>
2024-10-19 11:49:53 -0700

    Update edge_weight_self_loops.m

--- edge_weight_self_loops.m
+++ edge_weight_self_loops.m
@@ -1,3 +1,9 @@
+% Graph-based scalable sampling of 3D point cloud attributes
+% IEEE. Transactions on Image Processing
+% Author: Shashank N. Sridhara, University of Southern California
+
+%Function to compute self-loop weights for each block
+
 function self_loop_mat = edge_weight_self_loops(A, block_logical)
     % Computes the self loop matrix based on the edge weights going outside
     % the block defined by the vertices between first_point and last_point
